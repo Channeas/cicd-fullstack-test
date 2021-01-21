c15a02a60c3cecb382d70059ff7537ff6371842f
Johannes <johannes.idarsson@gmail.com>
2021-01-21 01:00:08 +0100

    lambda update test

--- backend/lambda/main/index.js
+++ backend/lambda/main/index.js
@@ -1,7 +1,9 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify("Hello from the backend Lambda!"),
+        body: JSON.stringify(
+            "Hello from the all new, completely remade backend Lambda!"
+        ),
     };
 
     return response;
